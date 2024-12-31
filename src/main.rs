afefedb2180d1e2f0278f6b75f514d659e5e29ee
HarshPatel2010 <harshpatel29301@gmail.com>
2024-12-31 15:51:46 +0530

    making api calls

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,26 @@
 use error_chain::error_chain;
 use std::io::Read;
+use serde::Deserialize;
+use reqwest::header::USER_AGENT;
+
+#[derive(Deserialize,Debug)]
+struct User{
+    login:String,
+    id:u32
+}
+
+#[tokio::main]
+async fn main()-> Result<()>{
+    let owner:String = "rust-lang-nursery".to_string();
+    let repo:String = "rust-cookbook".to_string();
+    let requestUrl:String = format!("https://api.github.com/repos/{owner}/{repo}/stargazers");
+    println!("{}",requestUrl);
+    let client = reqwest::Client::new();
+    let response = client.get(&requestUrl).header(USER_AGENT,"rust web api client demo task").send().await?;
+    let users:Vec<User>= response.json().await?;
+    println!(" Users  are---------------------{:?}",users);
+    Ok(())
+}
 
 
 error_chain! {
@@ -11,7 +32,7 @@ error_chain! {
     }
 }
 #[tokio::main]
-async fn main() -> Result<()> {
+async fn mxxain() -> Result<()> {
 let res=reqwest::get("https://www.rust-lang.org/en-US/").await?;
     println!("Status: {}", res.status());
     println!("Headers:\n{:#?}", res.headers());
