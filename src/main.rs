865f0b47e1f446b64dc90c31b3406d6d9e132f20
HarshPatel2010 <harshpatel29301@gmail.com>
2024-12-31 15:12:00 +0530

    tokio implemented for async functions

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use error_chain::error_chain;
 use std::io::Read;
 
+
 error_chain! {
     foreign_links {
         Io(
@@ -9,12 +10,26 @@ error_chain! {
         HttpRequest(reqwest::Error);
     }
 }
-fn main() -> Result<()> {
-   let mut res = reqwest::blocking::get("http://httpbin.org/get")?;
+#[tokio::main]
+async fn main() -> Result<()> {
+let res=reqwest::get("https://www.rust-lang.org/en-US/").await?;
+    println!("Status: {}", res.status());
+    println!("Headers:\n{:#?}", res.headers());
+    let body = res.text().await?;
+    println!("Body:\n{}", body);
+    println!("COmpleted");
+    Ok(())
+
+}
+
+fn xmain() -> Result<()> {
+    let mut res = reqwest::blocking::get("http://httpbin.org/get")?;
     let mut body= String::new();
     res.read_to_string(&mut body).unwrap();
     println!("status {}", res.status());
-println!("headers {:?}", res.headers());
+    println!("headers {:?}", res.headers());
     println!("body {}", body);
     Ok(())
 }
+
+
